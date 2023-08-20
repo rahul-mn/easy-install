51523ca8aaf276d91b885cbb2160515962d17698
Rahul meena <meenarahul67@gmail.com>
2023-08-20 18:14:04 +0530

    Create debian-vault-installer.sh

---
+++ debian-vault-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# GPG is required for the package signing key
+sudo apt update && sudo apt install gpg
+
+# Download the signing key to a new keyring
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Verify the key's fingerprint
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+
+# Add the HashiCorp repo
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# apt update!
+sudo apt update
+
+# Install a product
+sudo apt install vault -y
+
+# Check Version
+vault -v
